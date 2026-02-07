ba32f1a0594b32d2e0f06573e9b3f6c250ac3722
Deep <diphiwase0987@gmail.com>
2026-02-07 18:10:10 +0530

    wip

---
+++ js_group/Playground/Promises/Parallel_Operations/main.js
@@ -0,0 +1,70 @@
+// // Promise.resolve();  // immediately resolve and return promise
+// Promise.resolve(10).then(console.log);
+
+// Promise.reject("test error").catch(console.error);
+
+function fetchUsers(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("user");
+    }, delay);
+  });
+}
+function fetchPosts(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("posts");
+    }, delay);
+  });
+}
+function fetchComments(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("comments");
+    }, delay);
+  });
+}
+
+// Promise.all([fetchUsers(1000), fetchPosts(2000), fetchComments(300)]).then(
+//   (response) => {
+//     console.log(response);
+//   },
+// );
+
+// const startTime = performance.now();
+// console.time();
+// Promise.race([fetchUsers(10000), fetchPosts(2000), fetchComments(3000)])
+//   .then((response) => {
+//     console.log(response);
+//     console.timeEnd();
+//     const endTime = performance.now();
+//     console.log(endTime - startTime);
+//   })
+//   .catch();
+
+// const startTime = performance.now();
+
+// Promise.any([
+//   // nay one response which come fast even if its a error
+//   fetchUsers(10000),
+//   fetchPosts(2000),
+//   fetchComments(3000),
+//   new Promise((reject) => reject("error")),
+// ])
+//   .then((response) => console.error(response))
+//   .catch((err) => console.error);
+// // if all fail then it reject and goes to catch
+
+function fetch4() {}
+
+Promise.allSettled([
+  fetchUsers(1000),
+  fetchPosts(2000),
+  fetchComments(3000),
+  (() => new Promise((reject) => reject("Error")))(),
+])
+  .then((response) => console.log(response))
+  .catch(console.error);
+
+// if fullfilled -> value
+// if reject -> reason
---
+++ js_group/Projects/API_HEALTH_MONITOR/main.js
@@ -0,0 +1,65 @@
+function server1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Status OK, 200");
+    }, 4000);
+  });
+}
+function server2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Status OK, 200");
+    }, 1000);
+  });
+}
+function server3() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("Error, 4XX");
+    }, 3000);
+  });
+}
+function server4() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Status OK, 200");
+    }, 1000);
+  });
+}
+
+function checkAllSERVERStatus() {
+  return Promise.allSettled(
+    [server1, server2, server3, server4].map((server) => server()),
+  ).then((res) => {
+    console.table(res);
+    for (let i in res) {
+      if (res[i].status === "rejected") {
+        console.warn(
+          `server${+i + 1} failed with reason/error message${res[i].reason}`,
+        );
+      } else {
+        console.log(
+          `Server${+i + 1} is ok with status message ${res[i].value}`,
+        );
+      }
+    }
+  });
+}
+
+function checkFastestAvailableServer() {
+  return Promise.any(
+    [server1, server2, server3, server4].map((server) => server()),
+  );
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  checkAllSERVERStatus()
+    .then(checkFastestAvailableServer)
+    .then((res) => console.log(res));
+});
+
+console.table();
+
+Promise.all(); // check for all status
+Promise.any(); // check any one is fastest
+Promise.race(); // check if fastest one is in timout  -> if not error else success // server which one is fastest and timesout function with delay you want within
