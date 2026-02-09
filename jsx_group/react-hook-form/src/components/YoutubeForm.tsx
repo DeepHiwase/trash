dbb6a21c34e3bc196edb7ac90f6d218676f9264c
Deep <diphiwase0987@gmail.com>
2026-02-09 13:43:09 +0530

    react-hook-form devtools, form submission, registering

---
+++ jsx_group/react-hook-form/src/components/YoutubeForm.tsx
@@ -0,0 +1,51 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let renderCount = 0;
+
+export default function YoutubeForm() {
+  const form = useForm<FormValues>();
+
+  const { register, control, handleSubmit } = form;
+
+  // const { name, ref, onChange, onBlur } = register("username");
+
+  type FormValues = {
+    username: string;
+    email: string;
+    channel: string;
+  };
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data);
+  };
+
+  renderCount++;
+  return (
+    <div>
+      <h1>Youtube Form {renderCount / 2}</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          // name={name}
+          // ref={ref}
+          // onChange={onChange}
+          // onBlur={onBlur}
+          {...register("username")}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register("email")} />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register("channel")} />
+
+        <button>Submit</button>
+      </form>
+
+      <DevTool control={control} />
+    </div>
+  );
+}
